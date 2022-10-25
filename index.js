d5df70879f9de76d7123428303553a5690637c28
kobir0 <kxkobir1999@gmail.com>
2022-10-26 00:10:16 +0600

    For Each category Data Send

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
-const cetegory = require("./data/course.json")
+const cetegory = require("./data/course.json");
+const allCetegoryData = require("./data/courseDetails.json");
 
 app.get("/", (req, res) => {
     res.send({})
@@ -13,6 +14,11 @@ app.get("/", (req, res) => {
 
 app.get("/cetegory", (req, res) => {
     res.send(cetegory);
+})
+app.get("/cetegory/:id", (req, res) => {
+    const id = req.params.id;
+    const cetegoryItems = allCetegoryData.filter((p) => p.category == id);
+    res.send(cetegoryItems);
 })
 app.listen(Port, () => {
     console.log('server is running:', Port);
